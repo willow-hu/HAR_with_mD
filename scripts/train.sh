484a7be415cce7c43e2b8ef5cd316c96a1959371
willow-hu <willow_hu@outlook.com>
2023-04-29 15:24:05 +0800

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,6 +1,6 @@
 CONFIG_FILE='configs/resnet/resnet34_8xb32_md.py'
-WORK_DIR='work_dirs/microD_50train_r34_lr0.1'
+WORK_DIR='work_dirs/microD_50train_r34'
 
 python tools/train.py \
 $CONFIG_FILE \
---work-dir $WORK_DIR \ 
+--work-dir $WORK_DIR
